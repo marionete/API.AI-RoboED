275b39fe943c041b4f11e4eae85d3fc65a20befc
marionete <marionete@users.noreply.github.com>
2017-06-25 07:22:13 -0300

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@
 timeout = 5
 debug = False
 ChatADM = "-1001083924806" ### CHAT FROM ADM
-source = "ROBOED"
+source = "ed.compet.gov"
 WeatherKey = ""
 token = "423177099:AAGhY6jBPaqsWK2DmrwGxRePAenmNETPKRc" ## TOKEN KEY FROM BOOT FROM TELEGRAM
 telegram = "https://api.telegram.org/bot{token}/"
-telegram = "https://api.telegram.org/bot{token}/"AiApi = "http://www.{host}.br/mod_perl/bot_gateway.cgi?bot_id={BotID}&charset=utf-8&pure=1&js=0&msg={msg}"
+AiApi = "http://www.{host}.br/mod_perl/bot_gateway.cgi?bot_id={BotID}&charset=utf-8&pure=1&js=0&msg={msg}"
 WeatherRequest = "http://api.openweathermap.org/data/2.5/weather?q={cidade}&mode=json&units=metric&appid={key}"
